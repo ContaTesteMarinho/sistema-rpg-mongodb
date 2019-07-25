6644e646e3dd9cbeff660a6a6a50f74a676fa88d
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-07-25 13:09:30 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/feliphecosta/sistemarpgmongodb/SistemarpgmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.feliphecosta.sistemarpgmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemarpgmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemarpgmongodbApplication.class, args);
+	}
+
+}
